90c8d12cf1b22f9bcfc31236560ac62b853f7f1c
K.Sivabalan <ksivabalan.91@gmail.com>
2023-01-21 20:46:37 +0800

    executor service implementation

--- src/main/java/sdf/App.java
+++ src/main/java/sdf/App.java
@@ -1,5 +1,8 @@
 package sdf;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Hello world!
  */
@@ -12,6 +15,26 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run(){
+                for (int i =0;  i<20;i++){
+                    System.out.println(Thread.currentThread().getName()+"\tRunnable ..."+i);
+                }
+            }
+        });
+        thread1.start();
+            
+        MyRunnableImplementation mRI = new MyRunnableImplementation();
+        Thread thread2 = new Thread(mRI);
+        thread2.start();
+
+        Thread thread3 = new Thread(mRI);
+        thread3.start();
+
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.execute(mRI);
+        executorService.shutdown();
+
     }
 }
